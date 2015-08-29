b23231771185f768a9c378e8703b08ffed2cb548
Saniere <hooy91@gmail.com>
2015-08-29 12:50:20 +0000

    마이그레이션/모델파일 작성, 테이블 생성

---
+++ db/migrate/20150829123421_create_charts.rb
@@ -0,0 +1,21 @@
+class CreateCharts < ActiveRecord::Migration
+  def change
+    create_table :charts do |t|
+			t.integer	:user_id
+			t.string 	:category
+			t.string	:name
+			t.text		:main_symptom
+			t.text		:onset
+			t.text		:provoke
+			t.text		:quality
+			t.text		:radiation
+			t.integer	:severe
+			t.text		:time
+			t.string	:pain_area
+			t.text		:diagnose
+			t.text		:plan
+
+      t.timestamps
+    end
+  end
+end
